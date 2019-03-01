3e1b888d437584dbd3494a6c1ed08de85c0e6127
A.Theo <adryantheo@gmail.com>
2019-03-01 23:04:47 +0700

    Login

---
+++ resources/js/components/admin/AppAdmin.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <v-app>
+        <v-toolbar dark color="primary">
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>Login</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+           
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-content>
+        <router-view></router-view>
+        
+               
+        </v-content>
+         <v-footer class="pa-3">
+            <v-spacer></v-spacer>
+            <div>&copy; {{ new Date().getFullYear() }}</div>
+        </v-footer>
+    </v-app>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
