ef912d51bade642a6c14a35b0439c8252b7ca1d3
Adryan Theo <adryantheo@gmail.com>
2019-03-28 14:56:31 +0700

    Front End Search Transaksi Pengujung

--- resources/js/components/pengunjung/PengunjungHistory.vue
+++ resources/js/components/pengunjung/PengunjungHistory.vue
@@ -1,7 +1,51 @@
 <template>
-    <div>
-        <p>Histroy Transaksi Page</p>
-        <p>Still Under Development</p>
-        
-    </div>
+    
+        <v-layout align-center justify-center fill-height>
+          <v-flex xs12 sm8>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Cari Transaksi</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="plat" label="Plat" type="text" :rules="[rules.plat]"></v-text-field>
+                  
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Cari</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      agreement: false,
+      bio: '',
+      dialog: false,
+      plat: undefined,
+      form: false,
+      isLoading: false,
+      password: undefined,
+      phone: undefined,
+      rules: {
+        plat: v => (v || '').match(/@/) || 'Plat Nomor yang Anda Masukkan Salah',
+        required: v => !!v || 'This field is required'
+      }
+    }),
+    methods:{
+        login(){
+            
+        }
+    }
+  }
+</script>
