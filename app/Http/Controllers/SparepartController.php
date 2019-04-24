75e0ae24fe0f1e54bdc50640a4452b7984b71061
A.Theo <adryantheo@gmail.com>
2019-04-25 01:51:42 +0700

    Revert "Fix Edit Gambar Sparepart"
    This reverts commit bc6d7497304b70d329ab6946fba6322a0c2c0f4f.

--- app/Http/Controllers/SparepartController.php
+++ app/Http/Controllers/SparepartController.php
@@ -70,39 +70,26 @@ class SparepartController extends Controller
     }
 
     
-    public function update(Request $request, $sparepart)
+    public function update(Request $request, Sparepart $sparepart)
     {
-        // $status = $sparepart->update(
+        $status = $sparepart->update(
             
-        //     $request->only([
-        //         'Kode_Sparepart',
-        //         'Nama',
-        //         'Tipe',
-        //         'Merk',
-        //         'Jenis_Motor',
-        //         'Harga_Beli',
-        //         'Harga_Jual',
-        //         'Letak',
-        //         'Stok',
-        //         'Stok_Min',
-        //     ]);
-        // );
-
-        $status = Sparepart::find($sparepart);
-        $status->Kode_Sparepart= $request['Kode_Sparepart'];
-        $status->Nama= $request['Nama'];
-        $status->Tipe= $request['Tipe'];
-        $status->Merk= $request['Merk'];
-        $status->Jenis_Motor= $request['Jenis_Motor'];
-        $status->Harga_Jual= $request['Harga_Jual'];
-        $status->Letak= $request['Letak'];
-        $status->Stok= $request['Stok'];
-        $status->Stok_Min= $request['Stok_Min'];
-        $status->Gambar= $this->uploadGambar($request);
-        $status->update();
-
-
-
+            $request->only([
+                'Kode_Sparepart',
+                'Nama',
+                'Tipe',
+                'Merk',
+                'Jenis_Motor',
+                'Harga_Beli',
+                'Harga_Jual',
+                'Letak',
+                'Stok',
+                'Stok_Min',
+                '',            
+                                  
+        
+            ])
+        );
         return response()->json([
             'status' => $status,
             'message' => $status ? 'Sparepart Diupdate!' : 'Error Mengupdate Sparepart'
